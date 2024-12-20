040a315561be2d035ba5f0bb07223e7acbd94055
Dmitry Egorov <d.yegorov@gmail.com>
2024-12-20 16:46:38 +0300

    update groups

--- src/groups.ts
+++ src/groups.ts
@@ -10,15 +10,15 @@ type Group = {
 const GROUPS: Group[] = [
 	{
 		name: "BTC",
-		tokenIdList: [11, 12, 13, 14, 15],
+		tokenIdList: [11, 12, 13, 14, 15, 21, 22, 23, 24, 25],
 	},
 	{
 		name: "ETH",
-		tokenIdList: [3, 21, 22, 23, 24, 25],
+		tokenIdList: [3, 21, 22, 23, 24, 25, 12, 13, 14, 15],
 	},
 	{
 		name: "ETH L2",
-		tokenIdList: [22, 23, 21, 25, 24],
+		tokenIdList: [22, 23, 21, 25, 24, 15, 14, 13, 12, 11],
 	},
 	{
 		name: "L1 Networks",
