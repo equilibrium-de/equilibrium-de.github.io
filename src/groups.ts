a8c02db92713216c3ff7344b04b1296d647774e6
Dmitry Egorov <d.yegorov@gmail.com>
2024-12-24 17:02:13 +0300

    update groups

--- src/groups.ts
+++ src/groups.ts
@@ -2,6 +2,7 @@ import { hasUnique } from "./utils";
 import { VAULTS } from "./vaults";
 
 type Group = {
+	id: number;
 	name: string;
 	tokenIdList: number[];
 	rank?: number;
@@ -9,38 +10,47 @@ type Group = {
 
 const GROUPS: Group[] = [
 	{
+		id: 1,
 		name: "BTC",
 		tokenIdList: [11, 12, 13, 14, 15, 21, 22, 23, 24, 25],
 	},
 	{
+		id: 2,
 		name: "ETH",
 		tokenIdList: [3, 21, 22, 23, 24, 25, 12, 13, 14, 15],
 	},
 	{
+		id: 3,
 		name: "ETH L2",
 		tokenIdList: [22, 23, 21, 25, 24, 15, 14, 13, 12, 11],
 	},
 	{
+		id: 4,
 		name: "L1 Networks",
 		tokenIdList: [11, 12, 13, 14, 15],
 	},
 	{
+		id: 5,
 		name: "LP tokens",
 		tokenIdList: [11, 12, 13, 14, 15],
 	},
 	{
+		id: 6,
 		name: "Governance tokens",
 		tokenIdList: [11, 12, 13, 14, 15],
 	},
 	{
+		id: 7,
 		name: "Memes",
 		tokenIdList: [11, 12, 13, 14, 15],
 	},
 	{
+		id: 8,
 		name: "Stablecoins",
 		tokenIdList: [11, 12, 13, 14, 15],
 	},
 	{
+		id: 9,
 		name: "Others",
 		tokenIdList: [11, 12, 13, 14, 15],
 	},
