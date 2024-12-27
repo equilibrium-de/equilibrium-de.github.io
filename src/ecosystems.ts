0bcf39f91df93373ab85cad3dd79df0f077e77dd
Dmitry Egorov <d.yegorov@gmail.com>
2024-12-27 10:55:19 +0300

    feat: update ecosystems

--- src/ecosystems.ts
+++ src/ecosystems.ts
@@ -1,20 +1,26 @@
 export type Ecosystem = {
 	name: "evm" | "solana" | "aptos" | "sui"; // pk
 	displayName?: string;
+	subtitle?: string;
 };
 
 export const ECOSYSTEMS: Ecosystem[] = [
 	{
 		name: "evm",
+		displayName: "Ethereum + 94",
+		subtitle: "Arbitrum, BNB, Base, etc.",
 	},
 	{
 		name: "solana",
+		displayName: "Solana",
 	},
 	{
 		name: "sui",
+		displayName: "Sui",
 	},
 	{
 		name: "aptos",
+		displayName: "Aptos",
 	},
 ];
 
