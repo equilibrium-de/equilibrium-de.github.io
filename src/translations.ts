12136a25d4855fd01ddaac21cad9b502a71f041d
Dmitry Egorov <d.yegorov@gmail.com>
2025-04-30 12:16:50 +0300

    add translations

--- src/translations.ts
+++ src/translations.ts
@@ -183,19 +183,24 @@ const TRANSLATIONS = {
 	"Your pokeMON points": "你的pokeMON积分",
 	"For Staking": "质押奖励",
 	"For Frens": "朋友奖励",
-	"You are #{place} enjoyoor":	"你在enjoyoor排行榜排名第{place}",
-	"You are #{place} referrer":	"你在推荐人排行榜排名第{place}",
+	"You are #{place} enjoyoor": "你在enjoyoor排行榜排名第{place}",
+	"You are #{place} referrer": "你在推荐人排行榜排名第{place}",
 	"Invitation link copied": "已复制邀请链接",
 	"Top stakers": "排名最高的质押者",
 	"Top referrers": "排名最高的推荐人",
 	Top: "前",
-	"Connect your wallet and get 10% boost on your rewards": "连接你的钱包，额外获得10%的奖励",
+	"Connect your wallet and get 10% boost on your rewards":
+		"连接你的钱包，额外获得10%的奖励",
 	"You were invited by {address}": "{address}对你发出邀请",
 	"You can't use your own code": "你无法使用自己的邀请码",
 	"Already joined via referral link": "已经通过推荐链接加入",
-	"You got a 10% bonus on your rewards as an invited fren": "你受到别人邀请，因此可以额外获得10%的奖励",
+	"You got a 10% bonus on your rewards as an invited fren":
+		"你受到别人邀请，因此可以额外获得10%的奖励",
 	"Enter the correct email address": "请输入正确的电子邮箱地址",
 	"Thanks for becoming an enjoyoor!": "感谢加入Enjoyoors!",
+
+	"/img/hero.svg": "/img/hero-zh.svg",
+	"/img/hero-sm.svg": "/img/hero-sm-zh.svg",
 };
 
 const generate = () => {
