d9551129bacb941b0c33ade76bcf86fdd3fe4a33
Dmitry Egorov <d.yegorov@gmail.com>
2025-07-09 15:36:12 +0300

    wip: campaigns test api

--- src/campaigns.ts
+++ src/campaigns.ts
@@ -66,11 +66,19 @@ export const CAMPAIGNS: Campaign[] = [
 	},
 	{
 		name: "The10kSquad nft",
-		startedAt: "2025-07-03T01:02:03.456Z",
-		endedAt: "2026-07-03T01:02:03.456Z",
+		startedAt: "2025-07-08T01:00:00.000Z",
+		endedAt: "2026-07-29T01:00:00.000Z",
 		type: "NftHolder",
 		boost: 2,
 		nftContracts: ["0x3a9454c1b4c84d1861bb1209a647c834d137b442"],
+	},
+	{
+		name: "LilChogstars nft",
+		startedAt: "2025-07-11T01:00:00.000Z",
+		endedAt: "2026-08-01T01:00:00.000Z",
+		type: "NftHolder",
+		boost: 2,
+		nftContracts: ["0x26c86f2835c114571df2b6ce9ba52296cc0fa6bb"],
 	},
 ];
 
